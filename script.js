1dad5e37367e0afd8475e10e6eaaaaebbae4a040
Tejas Kumar S <tej0897@gmail.com>
2024-05-13 11:30:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
+const charRange  = (start, end) => range(start, end)
+
 window.onload = () => {
   const container = document.getElementById("container");
   const createLabel = (name) => {
     const label = document.createElement("div");
-
+    label.className = "label";
+    label.textContent = name;
+    container.appendChild(label);
   }
 }
