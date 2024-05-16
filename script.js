9897ef744487c3a51997718593e1b54cf05aa7b3
Tejas Kumar S <tej0897@gmail.com>
2024-05-16 09:17:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,6 +20,11 @@ const spreadsheetFunctions = {
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
 const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map(code => String.fromCharCode(code));
 
+const evalFormula = (x, cells) => {
+  const idToText = id => cells.find(cell => cell.id === id).value;
+
+}
+
 window.onload = () => {
   const container = document.getElementById("container");
   const createLabel = (name) => {
@@ -46,5 +51,7 @@ window.onload = () => {
 const update = event => {
   const element = event.target;
   const value = element.value.replace(/\s/g, "");
+  if (!value.includes(element.id) && value.startsWith('=')) {
 
+  }
 }
