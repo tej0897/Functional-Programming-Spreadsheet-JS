8538bfb0d5ca3b921d57ad759dbc113bb38fc484
Tejas Kumar S <tej0897@gmail.com>
2024-05-21 11:09:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ const infixToFunction = {
   "/": (x, y) => x / y,
 }
 
-const infixEval = (str, regex) => str.replace(regex, (_match, arg1, operator, arg2) => 
-infixToFunction[operator](arg1,arg2));
+const infixEval = (str, regex) => str.replace(regex, (_match, arg1, operator, arg2) => infixToFunction[operator](parseFloat(arg1), parseFloat(arg2)));
+
+const highPrecedence = str => {
+  const regex = /([\d.]+)([*\/])([\d.]+)/;
+  return regex.test(str);
+}
+console.log(highPrecedence("5*3"));
 
 const isEven = num => num % 2 === 0;
 const sum = nums => nums.reduce((acc, el) => acc + el, 0);
